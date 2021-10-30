920876cd5344bd8937fa4c6f118fe2ffe57eec5c
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2021-10-29 22:52:20 -0300

    feat(req-body):Update query bidy to recieve another actions

--- pages/api/mongodb/[dbName]/[collectionName].js
+++ pages/api/mongodb/[dbName]/[collectionName].js
@@ -4,8 +4,13 @@ export default async function handler(req, res) {
   const { dbName, collectionName } = req.query;
   const { body } = req;
 
+  const { action, data } = body;
+
   const collectionCloud = await getCollection(dbName, collectionName);
-  const response = await collectionCloud.insertOne(body);
+
+  const actions = { insertOne: collectionCloud.insertOne(data) };
+
+  const response = await actions[action];
 
   res.status(200).json(response);
 }
